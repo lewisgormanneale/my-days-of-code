69aaf045e9e2f7ebba9d983e03df858219e75037
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-12-17 09:54:47 +0000

    better fixed navbar width

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -28,65 +28,33 @@ export default function Settings() {
         <div className="settings-header">
           <h2>Settings</h2>
         </div>
-        <div className="profile-info">
-          <img
-            className="profile-image"
-            src={profile?.avatar_url}
-            alt={profile?.username}
-          ></img>
-          <div>
-            <h3>Name: {profile?.full_name}</h3>
-            <h3>GitHub Username: {profile?.username}</h3>
-            <p>
-              <a href={`https://github.com/` + profile?.username}>
-                View GitHub Profile
-              </a>
-            </p>
-            <p>
-              <a
-                href={
-                  `https://www.codewars.com/users/` + profile?.codewars_username
-                }
-              >
-                View Codewars Profile
-              </a>
-            </p>
-            <p>View Public My Days Of Code Profile (Coming Soon)</p>
-          </div>
+        <h3>Name: {profile?.full_name}</h3>
+        <p>Change Name:</p>
+        <h3>GitHub Username: {profile?.username}</h3>
+        <p>Change Codewars Username:</p>
+        <input
+          name="codewars-username"
+          id="codewars-username"
+          type="text"
+          placeholder={profile.codewars_username}
+          required
+          onChange={(e) => {
+            setNewCodewarsUsername(e.target.value);
+          }}
+        ></input>
+        <button onClick={editCodewarsUsername}>Change Codewars Username</button>
+        <div className="settings-header">
+          <h2>Privacy</h2>
         </div>
-        <div className="profile-edit-options">
-          <div className="settings-header">
-            <h2>Edit Account Details</h2>
-          </div>
-          <p>Change Name:</p>
-          <p>Change Profile Image URL:</p>
-          <p>Change Codewars Username:</p>
+        <div className="privacy-field">
+          <label htmlFor="profile-visibility">
+            Let Anyone View My Days Of Code Profile (Coming Soon)
+          </label>
           <input
-            name="codewars-username"
-            id="codewars-username"
-            type="text"
-            placeholder={profile.codewars_username}
-            required
-            onChange={(e) => {
-              setNewCodewarsUsername(e.target.value);
-            }}
-          ></input>
-          <button onClick={editCodewarsUsername}>
-            Change Codewars Username
-          </button>
-          <div className="settings-header">
-            <h2>Privacy</h2>
-          </div>
-          <div className="privacy-field">
-            <label htmlFor="profile-visibility">
-              Let Anyone View My Days Of Code Profile (Coming Soon)
-            </label>
-            <input
-              type="checkbox"
-              id="profile-visibility"
-              name="profile-visibility"
-            />
-          </div>
+            type="checkbox"
+            id="profile-visibility"
+            name="profile-visibility"
+          />
         </div>
       </div>
       <Footer />
